daa26bde6010def50cfd6c34d9deda9b22cf17dd
cka <cka@frontware.co.th>
2017-07-24 11:24:35 +0700

    [ticket/2] update code for department

--- Modules/Weladee_Attendances/weladee_pb2_grpc.py
+++ Modules/Weladee_Attendances/weladee_pb2_grpc.py
@@ -59,14 +59,19 @@ class MobileServicer(object):
     raise NotImplementedError('Method not implemented!')
 
   def GetHolidays(self, request, context):
-    """/ Stream of active holidays 1 employee including company holidays
+    """/ Stream of active holidays of an employee including company holidays
     """
     context.set_code(grpc.StatusCode.UNIMPLEMENTED)
     context.set_details('Method not implemented!')
     raise NotImplementedError('Method not implemented!')
 
   def GetLogEvent(self, request, context):
-    """/ Realtime event when employee check in/out. Get a stream for each in/out in the company depending on token or authorization. It can be limited to an employee, a team or entire company
+    """*
+    Realtime event when employee check in/out.
+    Get a stream for each in/out in the company depending on token or authorization.
+    It can be limited to an employee, a team or entire company.
+    The header contains "employees": an array of all employeeid who are IN at the moment the request was done.
+    Header example : "employees":["6762","451","152"]
     """
     context.set_code(grpc.StatusCode.UNIMPLEMENTED)
     context.set_details('Method not implemented!')
@@ -215,7 +220,13 @@ class OdooServicer(object):
     raise NotImplementedError('Method not implemented!')
 
   def UpdateEmployee(self, request, context):
-    """/ raise error if fails
+    """*
+    // updates the Employee from odoo in the database.
+    // Only few fields can be updated:
+    // phones, first_name_english, last_name_english, email, nickname_english,
+    // last_name_thai, active,  code, first_name_thai, nickname_thai
+    // positionid, photo.
+    // raise error if fails
     """
     context.set_code(grpc.StatusCode.UNIMPLEMENTED)
     context.set_details('Method not implemented!')
