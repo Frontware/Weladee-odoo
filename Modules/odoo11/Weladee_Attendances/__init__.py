ffa77f45bfd41c7a3cb7f07878e7a15c7fb02549
cka <cka@frontware.co.th>
2018-05-10 18:25:48 +0700

    [odoo11] update files on modules

---
+++ Modules/odoo11/Weladee_Attendances/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from . import weladee_pb2_grpc
+from . import weladee_pb2
+from . import odoo_pb2
+from . import odoo_pb2_grpc
+from . import weladee_employee
---
+++ Modules/odoo9/Weladee_Attendances/cka.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import grpc
+import weladee_pb2
+import weladee_pb2_grpc
+import os
+import datetime
+
+
+def main():
+    print("Test grpc weladee")
+
+    # Weladee grpc server address is grpc.weladee.com:22443
+    address = "grpc.weladee.com:22443"
+
+    # Define a secure channel with embedded public certificate
+
+    creds = grpc.ssl_channel_credentials(certificate)
+    channel = grpc.secure_channel(address, creds)
+
+    # Connect from Odoo
+    # Place here the token specific to each company. It's called api_key in table company
+
+    authorization = [("authorization", "1b004778-7e7b-403d-88c4-18227effc9d7")]
+
+    stub = weladee_pb2_grpc.OdooStub(channel)
+    myrequest = weladee_pb2.EmployeeRequest()
+
+
+    # List all departments
+    print("Departments")
+    if True:
+        for dept in stub.GetDepartments(myrequest, metadata=authorization):
+            print(dept)
+
+    # List of employees
+    print("----------------------------------")
+    print("Employees")
+    if True :
+        for emp in stub.GetEmployees(weladee_pb2.Empty(), metadata=authorization):
+            for emp in stub.GetEmployees(weladee_pb2.Empty(), metadata=authorization):
+                if emp :
+                    if emp.odoo :
+                        if not emp.odoo.odoo_id :
+                            print(emp)
+
+    # List of GetPositions
+    print("----------------------------------")
+    print("Positions")
+    if True :
+        for pos in stub.GetPositions(weladee_pb2.Empty(), metadata=authorization):
+            print(pos)
+
+    # List of CompanyHolidays
+    print("CompanyHolidays")
+    if True :
+        for ch in stub.GetCompanyHolidays(weladee_pb2.Empty(), metadata=authorization):
+            print(ch)
+    print("----------------------------------")
+    # List of CompanyHolidays
+    print("Holidays")
+    if False :
+        try:
+            for ch in stub.GetHolidays(weladee_pb2.Empty(), metadata=authorization):
+                print(ch)
+        except Exception as e:
+            print("Error when get Holidays", e)
+
+    # List of attendance to sync
+    print("Attendance to sync")
+    if False :
+        for att in stub.GetNewAttendance(weladee_pb2.Empty(), metadata=authorization):
+            if att.logevent :
+                print("------------")
+                print(att)
+                if att.logevent.timestamp :
+                    print(att.logevent.timestamp)
+                    print(
+                        datetime.datetime.fromtimestamp(
+                            att.logevent.timestamp
+                        ).strftime('%Y-%m-%d %H:%M:%S')
+                    )
+
+
+
+
+
+# Certificate to be downloaded from https://git.frontware.co.th/raw/Weladee/proto.git/master/certificates!grpc.weladee.com.chain.crt
+
+
+certificate = """-----BEGIN CERTIFICATE-----
+MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/
+MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT
+DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow
+SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT
+GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC
+AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF
+q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8
+SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0
+Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA
+a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj
+/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T
+AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG
+CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv
+bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k
+c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw
+VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC
+ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz
+MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu
+Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF
+AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo
+uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/
+wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu
+X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG
+PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6
+KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/
+MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT
+DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow
+PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD
+Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
+AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O
+rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq
+OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b
+xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw
+7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD
+aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV
+HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG
+SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69
+ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr
+AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz
+R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5
+JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo
+Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ
+-----END CERTIFICATE-----
+"""
+
+
+if __name__ == "__main__":
+    import sys
+
+    # if --optimize then restart application with -O parameter.
+    if '--optimize' in sys.argv:
+        sys.argv.remove('--optimize')
+        os.execl(sys.executable, sys.executable, '-O', *sys.argv)
+    else:
+        main()
