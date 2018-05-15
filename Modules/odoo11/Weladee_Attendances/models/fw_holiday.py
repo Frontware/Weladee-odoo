282853504b43d6de94639e353afe7985436af604
cka <cka@frontware.co.th>
2018-05-15 09:20:06 +0700

    [odoo11] create company holiday

--- Modules/odoo11/Weladee_Attendances/models/fw_holiday.py
+++ Modules/odoo11/Weladee_Attendances/models/fw_holiday.py
@@ -19,6 +19,7 @@
 #
 ##############################################################################
 from odoo import models, fields, api
+from odoo import exceptions
 
 import datetime
 import time
@@ -28,10 +29,27 @@ class hr_holidays(models.TransientModel):
     _description="Weladee company holidays"
     
     company_holiday_description = fields.Char(string='Description', required=True)
-    company_holiday_date_from = fields.Date(string='From', required=True)
-    company_holiday_date_to = fields.Date(string='To', required=True)
+    company_holiday_date = fields.Date(string='Date', required=True, default=fields.Date.today)
     company_holiday_active = fields.Boolean("Active", default=False)
     company_holiday_notes = fields.Text('Notes')
+
+    @api.onchange('company_holiday_date')
+    def _onchange_company_holiday_date(self):
+         holiday = self.search([('company_holiday_date','=',self.company_holiday_date)])
+         if holiday:
+            raise exceptions.UserError('This Date already company holiday.')
+
+    @api.model
+    def create(self, vals):
+        print(vals["company_holiday_date"])
+        holiday_line_obj = self.env['weladee_attendance.company.holidays']
+        holiday_line_ids = holiday_line_obj.search( [ ('company_holiday_date','=', vals["company_holiday_date"] )] )
+
+        if holiday_line_ids :
+            raise exceptions.UserError('This Date already company holiday.')
+        else:
+            holiday = super(hr_holidays,self).create(vals)
+            return holiday
    
                  
 hr_holidays()
