ef6168d43c8ea645c186e34c280a30836219000e
kpo <kpo@frontware.com>
2018-11-09 10:27:22 +0700

    [MOVED] move folder

---
+++ Modules/Weladee_Attendances/library/weladee_translation.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo.tools.translate import _
+def allocate_holiday_tag(): return _(' employee tag %s')#4
