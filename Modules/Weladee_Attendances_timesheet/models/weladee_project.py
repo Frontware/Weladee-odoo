90d51594bbb54cc136fb8d1049a0a153d8ef803a
kpo <kpo@frontware.com>
2022-07-22 13:04:30 +0700

    [split] timesheet

---
+++ Modules/Weladee_Attendances_timesheet/models/weladee_project.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+_logger = logging.getLogger(__name__)
+
+from odoo import osv
+from odoo import models, fields, api
+
+class weladee_project(models.Model):
+    _inherit = 'project.project'
+
+    weladee_id = fields.Char(string="Weladee ID",copy=False)
+    descrition = fields.Html(translate=True)
+    url = fields.Char('URL')
+    note = fields.Text('Note')
+
+    @api.model
+    def create(self, vals):
+        name_th = vals.get('name-th', '') 
+        des_th = vals.get('description-th', '')
+        if 'name-th' in vals: del vals['name-th']
+        if 'description-th' in vals: del vals['description-th']
+        ret = super(weladee_project, self).create(vals)
+        irobj = self.env['ir.translation']
+
+        irobj._set_ids('project.project,name','model','en_US', [ret.id], vals.get('name', ''))
+        irobj._set_ids('project.project,name','model','th_TH', [ret.id], name_th)
+
+        irobj._set_ids('project.project,description','model','en_US', [ret.id], vals.get('description', ''))
+        irobj._set_ids('project.project,description','model','th_TH', [ret.id], des_th)
+
+        return ret
