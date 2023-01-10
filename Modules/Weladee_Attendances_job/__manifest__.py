420e1203a699f8020a7860ff95795676ca3e53b0
kpo <kpo@frontware.co.th>
2023-01-10 16:42:54 +0700

    [migration] Weladee_Attendances_job to 16.0

--- Modules/Weladee_Attendances_job/__manifest__.py
+++ Modules/Weladee_Attendances_job/__manifest__.py
@@ -1,26 +1,7 @@
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution    
-#    Copyright (C) 2004-Now Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    d$
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
 {
 "name" : "Weladee attendances job module",
-"version" : "5.00",
+"version" : "6.00",
 "author" : "Frontware International",
 "category" : "Generic Modules",
 'summary': 'Weladee-Odoo attendances\'s module',
@@ -33,8 +14,12 @@
         'views/fw_hr_job.xml',
         'views/fw_hr_job_applicant.xml',
         'views/fw_weladee_job_ads.xml',
-        'templates/asset_Weladee_Attendances.xml',
 ],
+'assets': {
+  'web.assets_backend': [
+      '/Weladee_Attendances_job/static/src/css/fw_jobweladee.css'
+  ]
+},
 "installable" : True,
 "active" : False,
 "website" : "https://www.weladee.com/",
@@ -44,5 +29,15 @@ Weladee attendances job module
 Module to manage synchronous job.
 
 It will synchronus job to odoo.
+
+job positions
+- tab job ads
+
+Recruitment
+- weladee jobads
+
+change log:
+------------------------------------
+* 2023-01-18 KPO (6.0) updated to odoo 16.0
 """
 }
--- Modules/Weladee_Attendances_job/models/weladee_job_app.py
+++ Modules/Weladee_Attendances_job/models/weladee_job_app.py
@@ -27,6 +27,11 @@ class weladee_job_app(models.Model):
     note = fields.Text(string='Note')
     hide_edit_btn_css = fields.Html(string='css', sanitize=False, compute='_compute_css')
 
+    def unlink(self):
+        self.env['weladee_attendance.synchronous'].check_weladee_id(self, {})
+
+        return super(weladee_job_app, self).unlink()
+
     def open_weladee_job_app(self):
         if self.weladee_url:
             return {
