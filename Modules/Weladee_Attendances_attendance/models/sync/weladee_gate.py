f8e5f54b5de05967539178e715c0c569224d2046
kpo <kpo@frontware.co.th>
2023-08-17 14:42:12 +0700

    [fix] allow gate change

--- Modules/Weladee_Attendances_attendance/models/sync/weladee_gate.py
+++ Modules/Weladee_Attendances_attendance/models/sync/weladee_gate.py
@@ -50,7 +50,7 @@ def sync_gate(self, req):
             odoo_pos = sync_gate_data(weladee_gate, req)
 
             if odoo_pos and odoo_pos['res-mode'] == 'create':
-               newid = req.gate_obj.create(sync_clean_up(odoo_pos))
+               newid = req.gate_obj.sudo().create(sync_clean_up(odoo_pos))
                sync_logdebug(req.context_sync, "Insert gate '%s' to odoo" % odoo_pos['name'] )
                sync_stat_create(req.context_sync['stat-gate'], 1)
 
@@ -59,7 +59,7 @@ def sync_gate(self, req):
             elif odoo_pos and odoo_pos['res-mode'] == 'update':
                 odoo_id = req.gate_obj.search([('id','=',odoo_pos['res-id'])])
                 if odoo_id.id:
-                   odoo_id.write(sync_clean_up(odoo_pos))
+                   odoo_id.sudo().write(sync_clean_up(odoo_pos))
                    sync_logdebug(req.context_sync, "Updated gate '%s' to odoo" % odoo_pos['name'] )
                    sync_stat_update(req.context_sync['stat-gate'], 1)
 
