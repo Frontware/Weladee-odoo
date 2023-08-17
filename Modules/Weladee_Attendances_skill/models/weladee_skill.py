5a93e75044a9841754a8f6478a9de5234369ef27
kpo <kpo@frontware.co.th>
2023-08-17 15:30:19 +0700

    [fix] skill

--- Modules/Weladee_Attendances_skill/models/weladee_skill.py
+++ Modules/Weladee_Attendances_skill/models/weladee_skill.py
@@ -28,11 +28,11 @@ class weladee_skill(models.Model):
         return super(weladee_skill, self).unlink()
     
     def open_weladee_skill(self):
-        if self.weladee_url:
+        if self.weladee_id:
             return {
                 'name': _('Skill'),
                 'type': 'ir.actions.act_url',
-                'url': 'https://www.weladee.com/skills/%s' % self.weladee_id,
+                'url': 'https://www.weladee.com/skill/skill/%s' % self.weladee_id,
                 'target': 'new'
             }
         else:
